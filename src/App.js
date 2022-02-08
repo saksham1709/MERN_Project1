f199594a962559427f73e886507a5136f0c2be6b
Saksham <sakshamsharma1709@gmail.com>
2022-02-08 21:46:07 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
+import Navbar from './components/navbar.component';
+import ExercisesList from './components/exercises-list.component';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import EditExercise from './components/edit-exercise.component';
+import CreateExercise from './components/create-exercise.component';
+import CreateUser from './components/create-user.component';
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="container">
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<ExercisesList/>}/>
+        </Routes>
+        <Routes>
+          <Route path="/edit/:id" element={<EditExercise/>}/>
+        </Routes>
+        <Routes>
+          <Route path="/create" element={<CreateExercise/>} />
+        </Routes>
+        <Routes>
+          <Route path="/user" element={<CreateUser/>} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
-
-export default App;
